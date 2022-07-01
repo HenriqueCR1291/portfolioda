1cf665b4bb713270117c1e9e3c213c5a51f0ff41
Henrique Ribeiro <72476806+HenriqueCR1291@users.noreply.github.com>
2022-06-30 21:52:32 -0300

    Add files via upload

--- Query_nashvillehousing.sql
+++ Query_nashvillehousing.sql
@@ -32,3 +32,68 @@ JOIN Portfolio_DA..nashville_housing b
 	ON a.ParcelID = b.ParcelID
 	AND a.[UniqueID ] <> b.[UniqueID ]
 WHERE a.PropertyAddress IS NULL
+
+
+--Breaking address in address, city
+SELECT PropertyAddress FROM Portfolio_DA..nashville_housing
+
+SELECT SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) AS Address,
+SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress)) AS City FROM Portfolio_DA..nashville_housing
+
+ALTER TABLE Portfolio_DA..nashville_housing
+ADD PropertySplitAddress Nvarchar(255)
+
+UPDATE Portfolio_DA..nashville_housing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1)
+
+ALTER TABLE Portfolio_DA..nashville_housing
+ADD PropertySplitCity Nvarchar(255)
+
+UPDATE Portfolio_DA..nashville_housing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress))
+
+
+SELECT PARSENAME(REPLACE(OwnerAddress, ',','.'), 3),
+PARSENAME(REPLACE(OwnerAddress, ',','.'), 2),
+PARSENAME(REPLACE(OwnerAddress, ',','.'), 1)
+FROM Portfolio_DA..nashville_housing
+
+ALTER TABLE Portfolio_DA..nashville_housing
+ADD OwnerSplitAddress Nvarchar(255)
+
+UPDATE Portfolio_DA..nashville_housing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',','.'), 3)
+
+ALTER TABLE Portfolio_DA..nashville_housing
+ADD CitySplitAddress Nvarchar(255)
+
+UPDATE Portfolio_DA..nashville_housing
+SET CitySplitAddress = PARSENAME(REPLACE(OwnerAddress, ',','.'), 2)
+
+ALTER TABLE Portfolio_DA..nashville_housing
+ADD StateSplitAddress Nvarchar(255)
+
+UPDATE Portfolio_DA..nashville_housing
+SET StateSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',','.'), 1)
+
+SELECT * FROM Portfolio_DA..nashville_housing
+
+
+-- SoldasVacant
+SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant) 
+FROM Portfolio_DA..nashville_housing
+GROUP BY SoldAsVacant
+ORDER BY 2 DESC
+
+SELECT SoldAsVacant,
+CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+	 WHEN SoldAsVacant = 'N' THEN 'No'
+	 ELSE SoldAsVacant
+	 END
+FROM Portfolio_DA..nashville_housing
+
+UPDATE Portfolio_DA..nashville_housing
+SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+	 WHEN SoldAsVacant = 'N' THEN 'No'
+	 ELSE SoldAsVacant
+	 END
